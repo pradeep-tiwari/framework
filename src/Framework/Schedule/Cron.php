a82f1dedbf3aa9bfe742a286fec8553bed4eaa89
Pradeep Tiwari <pt21388@gmail.com>
2022-07-31 12:55:01 +0530

    Scheduler in progress

--- src/Framework/Schedule/Cron.php
+++ src/Framework/Schedule/Cron.php
@@ -9,125 +9,96 @@ class Cron
      */
     protected $cronTime;
 
+    protected $minutes;
+    protected $hours;
+    protected $days;
+    protected $months;
+    protected $weekdays;
+
     /**
      * @var string $cronTime Cron time expression
      */
-    public function isDue(string $cronTime)
+    public function __construct(string $cronTime)
     {
         $this->cronTime = $cronTime;
 
         // Split cron time expression into array
         $cronParts = explode(' ', $this->cronTime);
 
-        // Get cron seconds: 0-59
-        $cronSecond = $cronParts[0];
+        if (count($cronParts) !== 5) {
+            throw new \Exception('Invalid cron time expression');
+        }
 
         // Get cron minutes: 0-59
-        $cronMinute = $cronParts[1];
+        $this->minutes = $cronParts[0];
 
         // Get cron hours: 0-23
-        $cronHour = $cronParts[2];
+        $this->hours = $cronParts[1];
 
         // Get cron day of month: 1-31
-        $cronDayOfMonth = $cronParts[3];
+        $this->days = $cronParts[2];
 
         // Get cron month: 0-11 or JAN-DEC
-        $cronMonth = $cronParts[4];
+        $this->months = $cronParts[3];
 
         // Get cron day of week: 1-7 or SUN-SAT
-        $cronDayOfWeek = $cronParts[5];
+        $this->weekdays = $cronParts[4];
+    }
 
-        // Get cron year: 1970-2099 or NONE
-        $cronYear = $cronParts[6];
+    public function minuteIsDue()
+    {
+        return $this->checkIfDue($this->minutes, date('i'));
+    }
 
-        // Check if cron year is set
-        if ($cronYear == 'NONE') {
-            $cronYear = date('Y');
-        }
+    public function hourIsDue()
+    {
+        return $this->checkIfDue($this->hours, date('H'));
+    }
 
-        // Check if cron month is set
-        if ($cronMonth == '*') {
-            $cronMonth = date('m');
-        }
+    public function dayIsDue()
+    {
+        return $this->checkIfDue($this->days, date('j'));
+    }
 
-        // Check if cron day of month is set
-        if ($cronDayOfMonth == '*') {
-            $cronDayOfMonth = date('d');
-        }
+    public function monthIsDue()
+    {
+        return $this->checkIfDue($this->months, date('n'));
+    }
 
-        // Check if cron hour is set
-        if ($cronHour == '*') {
-            $cronHour = date('H');
-        }
+    public function weekdayIsDue()
+    {
+        return $this->checkIfDue($this->weekdays, date('w'));
+    }
 
-        // Check if cron minute is set
-        if ($cronMinute == '*') {
-            $cronMinute = date('i');
-        }
+    public function isDue()
+    {
+        return $this->minuteIsDue() && $this->hourIsDue() && $this->dayIsDue() && $this->monthIsDue() && $this->weekdayIsDue();
+    }
 
-        // Check if cron seconds is set
-        if ($cronSecond == '*') {
-            $cronSecond = date('s');
-        }
-
-        // Check if cron day of week is set
-        if ($cronDayOfWeek == '*') {
-            $cronDayOfWeek = date('w');
-        }
-
-        // Check if cron seconds is due
-        if ($cronSecond == date('s')) {
-            $cronSecond = true;
-        } else {
-            $cronSecond = false;
-        }
-
-        // Check if cron minute is due
-        if ($cronMinute == date('i')) {
-            $cronMinute = true;
-        } else {
-            $cronMinute = false;
-        }
-
-        // Check if cron hour is due
-        if ($cronHour == date('H')) {
-            $cronHour = true;
-        } else {
-            $cronHour = false;
-        }
-
-        // Check if cron day of month is due
-        if ($cronDayOfMonth == date('d')) {
-            $cronDayOfMonth = true;
-        } else {
-            $cronDayOfMonth = false;
-        }
-
-        // Check if cron month is due
-        if ($cronMonth == date('m')) {
-            $cronMonth = true;
-        } else {
-            $cronMonth = false;
-        }
-
-        // Check if cron year is due
-        if ($cronYear == date('Y')) {
-            $cronYear = true;
-        } else {
-            $cronYear = false;
-        }
-
-        // Check if cron day of week is due
-        if ($cronDayOfWeek == date('w')) {
-            $cronDayOfWeek = true;
-        } else {
-            $cronDayOfWeek = false;
-        }
-
-        // Check if cron is due
-        if ($cronSecond && $cronMinute && $cronHour && $cronDayOfMonth && $cronMonth && $cronYear && $cronDayOfWeek) {
+    protected function checkIfDue($expression, $current)
+    {
+        if ($expression === '*') {
             return true;
         }
+
+        if ($expression === $current) {
+            return true;
+        }
+
+        if (strpos($expression, '-') !== false) {
+            $parts = explode('-', $expression);
+            if ($current >= $parts[0] && $current <= $parts[1]) {
+                return true;
+            }
+        }
+
+        if (strpos($expression, '/') !== false) {
+            $parts = explode('/', $expression);
+            if ($current % $parts[1] === 0) {
+                return true;
+            }
+        }
+
         return false;
     }
 }

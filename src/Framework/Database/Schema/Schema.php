f436ebecc508270939e28d8cf6282fa89e21a85b
Pradeep Tiwari <pt21388@gmail.com>
2022-10-24 16:16:42 +0530

    Added support for column fields

--- src/Framework/Database/Schema/Schema.php
+++ src/Framework/Database/Schema/Schema.php
@@ -32,7 +32,7 @@ class Schema
     public function createTable(Table $table): void
     {
         $sql = (new CreateTable)->compile($table);
-        pp($sql);
+        
         $this->connection->query($sql);
     }
 
